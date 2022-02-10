1e4fe3b278569a1fbd86875ded4292e9f8559660
ingenii1 <boris.vega.ieei@gmail.com>
2022-02-10 13:20:53 +0000

    first commit, CRUD APP: copy & read, missing update and delete

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const yargs = require("yargs");
+const { client, connection } = require("./db/connection");
+const { addMovie } = require("./utils");
+
+const app = async (yargsObj) => {
+  try {
+    const collection = await connection();
+    if (yargsObj.add) {
+      await addMovie(collection, {
+        title: yargsObj.title,
+        actor: yargsObj.actor,
+      });
+      //add movie to mongodb database, needs collection and success message
+    } else {
+      console.log("Incorrect command");
+    }
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app(yargs.argv);
